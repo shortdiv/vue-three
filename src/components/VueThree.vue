148bb047cbd417fd84e8ce556beb37ea8009a62e
Michel EDIGHOFFER <edimitchel@gmail.com>
2019-08-27 09:02:04 +0200

    Use arrow function which returns promise from fetch

--- src/components/VueThree.vue
+++ src/components/VueThree.vue
@@ -38,7 +38,7 @@ export default {
     const point = value(null);
     const pointTwo = value(null);
     const dataLoaded = value(false);
-    const getMarathonRoute = function() {
+    const getMarathonRoute = () =>
       fetch(
         "https://gist.githubusercontent.com/shortdiv/cc38c54c49e153493835827e9fb05c6d/raw/a561954794bf303a9275a6c03f6f930f105767ba/boston_marathon.geojson"
       )
@@ -46,8 +46,7 @@ export default {
         .then(data => {
           pointTwo.value = data;
         });
-    };
-    const get5ksegment = function() {
+    const get5ksegment = () =>
       fetch(
         "https://gist.githubusercontent.com/shortdiv/97d74e464a8900853bc89048a8244c84/raw/e9bbb80410f798e82171dfdf453e232ef251a4fc/BostonMarathon_firstsegment.geojson"
       )
@@ -57,7 +56,6 @@ export default {
         .then(data => {
           point.value = data;
         });
-    };
     onCreated(() => {
       mapboxgl.accessToken =
         "pk.eyJ1IjoiYWxscnlkZXIiLCJhIjoidWs5cUFfRSJ9.t8kxvO3nIhCaAl07-4lkNw";
